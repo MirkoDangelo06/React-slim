b7fcce3dd7abf562899d8d44ff532f82b6267010
lai2-24 <lai2-24@itismeucci.com>
2025-05-13 12:33:08 +0200

    array and button

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import{useState} from "react";
 
 function App() {
 
-  const alunni =[{"id":"1","nome":"Claudio","cognome":"Benvenuti","scuola_id":"1"},{"id":"2","nome":"Ivan","cognome":"Bruno","scuola_id":"1"}];
+  const[alunni,setAlunni] = useState([]);
+
+  function caricaAlunni(){
+    const a = [{"id":"1","nome":"Claudio","cognome":"Benvenuti","scuola_id":"1"},{"id":"2","nome":"Ivan","cognome":"Bruno","scuola_id":"1"}];
+    setAlunni(a);
+  }
 
 
   return (
     <div className="App">
     
-    <table border="1">
-    {alunni.map(a =>
-      <tr>   
-        <td>{a.id}</td>
-        <td>{a.nome}</td>
-        <td>{a.cognome}</td>
-      </tr>
-    )}
-   </table>
+    {alunni.length > 0 ? (
 
+      <table border="1">
+      {alunni.map(a =>
+        <tr>   
+          <td>{a.id}</td>
+          <td>{a.nome}</td>
+          <td>{a.cognome}</td>
+        </tr>
+      )}
+    </table>
+      ):(
+        <button onClick={caricaAlunni}> carica alunni</button>
+      )
+    }
     </div>
   );
 }
